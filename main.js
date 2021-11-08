246794248bc89b2093e35253c2e845022235d6c8
ElenaLj <elenaljubijankic@gmail.com>
2021-11-08 17:35:37 +0100

    chiusura menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 //dichiarazione variabili
-const menu = document.querySelector('.fa-bars');
+const openMenu = document.querySelector('.fa-bars');
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
 //apertura menu
-menu.addEventListener("click", function(){
-    hamburgerMenu.classList.toggle("active");
+openMenu.addEventListener("click", function(){
+    hamburgerMenu.classList.add("active");
+});
+
+//chiusura menu
+const closeMenu = document.querySelector('.fa-times');
+
+closeMenu.addEventListener("click", function(){
+    hamburgerMenu.classList.remove("active");
 });
